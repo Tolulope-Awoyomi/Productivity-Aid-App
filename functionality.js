7c9b6f0b9c95e2c93dfc85d4bed533e4ab73de12
+ <+>
2022-12-27 21:21:42 -0500

    Modify buildActivities function to include a delete button

--- functionality.js
+++ functionality.js
@@ -2,21 +2,22 @@ document.addEventListener("DOMContentLoaded", () => {
     let form = document.getElementById('create-activity-form')
     form.addEventListener('submit', (e) => {
         e.preventDefault()
-        const newActivityDescription = document.getElementById("new-activity-description")
-        const newActivity = document.createElement('li')
-        newActivity.innerText = newActivityDescription.value
-        myActivities(newActivity)
+        buildActivities(e.target.new_activity_description.value)
+
+       
     })
 
-    function myActivities(activity){
-        const activity = document.getElementById("activities").appendChild(activity)
-        const p = document.createElement('p')
-        const btn = document.createElement('button')
-        btn.addEventListener('click', handleDelete)
-        btn.textContent = 'x'
-        p.textContent = `${activity}`
-        p.appendChild(btn)
-        console.log(p)
-        document.querySelector("activities").appendChild(p)
+    function buildActivities(activity){
+       let li = document.createElement('li')
+       let btn = document.createElement('button')
+       btn.addEventListener('click', handleDelete)
+       btn.textContent = ' delete'
+       li.textContent = `${activity} `    
+       li.appendChild(btn)
+       document.querySelector('#activities').appendChild(li)
     }
 });
+
+
+
+ 
