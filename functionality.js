33c0410e25cecefcf283d574d6d2a90e0c87a81a
+ <+>
2022-12-27 22:26:26 -0500

    Include a pause and play timer functionality

--- functionality.js
+++ functionality.js
@@ -3,31 +3,66 @@ document.addEventListener("DOMContentLoaded", () => {
     form.addEventListener('submit', (e) => {
         e.preventDefault()
         buildActivities(e.target.new_activity_description.value)
-        counter(e.target.new_activity_description.value)
         form.reset()
     })
-
-    function buildActivities(activity){
-       let li = document.createElement('li')
-       let btn = document.createElement('button')
-       btn.addEventListener('click', handleDelete)
-       btn.textContent = ' delete'
-       li.textContent = `${activity} `    
-       li.appendChild(btn)
-       document.querySelector('#activities').appendChild(li)
-    }
 });
+
+function buildActivities(activity){
+    let li = document.createElement('li')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', handleDelete)
+    btn.textContent = ' delete'
+    li.textContent = `${activity} `    
+    li.appendChild(btn)
+    document.querySelector('#activities').appendChild(li)
+    btn.addEventListener('click', timer)
+ }
 
 function handleDelete(e){
     e.target.parentNode.remove()
 }
 
-let intervalID = setInterval(counter, 1000);
-function counter() {
-    let counter = document.getElementById('timer')
-    let parsedNumber = parseInt(counter.innerText);
+let intervalID = setInterval(timer, 1000);
+function timer () {
+    let timer = document.getElementById("timer")
+    let parsedNumber = parseInt(timer.innerText);
     console.log (parsedNumber)
-    counter.innerText = parsedNumber +=1
-}
-
- 
+    timer.innerText = parsedNumber +=1
+  }
+  
+  let minus = document.getElementById("minus")
+  
+  minus.addEventListener("click", function () {
+  let timer = document.getElementById("timer")
+    let parsedNumber = parseInt(timer.innerText);
+    console.log (parsedNumber)
+    timer.innerText = parsedNumber -=1
+  })
+  
+  plus.addEventListener("click", function () {
+      let timer = document.getElementById("timer")
+      let parsedNumber = parseInt(timer.innerText);
+      console.log (parsedNumber)
+      timer.innerText = parsedNumber +=1
+      })
+  
+  const pause = document.getElementById("pause");
+  let minusButton = document.getElementById("minus");
+  let plusButton = document.getElementById("plus");
+  
+  pause.addEventListener("click", function(event) {
+      if (event.target.id === "resume") {
+          intervalID = setInterval(timer, 1000);
+          minusButton.removeAttribute("disabled")
+          plusButton.removeAttribute("disabled")
+          event.target.id = "pause"
+          event.target.textContent = "pause timer"
+      }
+      else {
+          clearInterval(intervalID);
+          minusButton.setAttribute("disabled", true)
+          plusButton.setAttribute("disabled", true)
+          event.target.id = "resume"
+          event.target.textContent = "resume timer"
+      }
+   });
